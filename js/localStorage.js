09a4b3f59890ae20dee05a7d51e2a9b1e8fb2617
Drimuar <Drimuar@gmail.com>
2022-05-20 13:17:38 +0700

    add ??, and style code

--- js/localStorage.js
+++ js/localStorage.js
@@ -6,26 +6,13 @@ export const storage = {
 	},
 	getFavoriteCities() {
 		const cities = localStorage.getItem("favoriteCities");
-		if (cities) {
-			return JSON.parse(cities);
-		} else {
-			return {};
-		}
-	},
-	getCurrentCity() {
-		//1 return localStorage.getItem("currentCity");
-		//2 const name = "currentCity";
-		//2 let matches = document.cookie.match(new RegExp(
-		//2 	"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-		//2 ));
-		//2 return matches ? decodeURIComponent(matches[1]) : undefined;
-		return Cookies.get("currentCity");
+		return JSON.parse(cities) ?? {};
 	},
 	saveCurrentCity(city) {
-		//1 localStorage.setItem("currentCity", city);
-		//2 const currentCity = "currentCity";
-		//2 document.cookie = encodeURIComponent(currentCity) + "=" + encodeURIComponent(city) + ";max-age=60;";
 		const inMinute = 1 / 5760;
 		Cookies.set("currentCity", city, { expires: inMinute });
+	},
+	getCurrentCity() {
+		return Cookies.get("currentCity");
 	},
 }
